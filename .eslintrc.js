95a6115b9b9003b44a06096cc5dc9b65820327f5
Yann VR <yannvr@gmail.com>
2018-11-29 02:11:08 +0000

    Add eslint with airbnb and turned down some rules

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: "airbnb",
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    semi: 0,
+    "no-param-reassign": "off",
+    quotes: ["warn", "double"],
+    "arrow-parens": [2, "as-needed"],
+  },
+}
